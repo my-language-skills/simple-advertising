cd98c0726a6a006f49743f69c2c8db03b916ecc9
davideC00 <dvdx97@gmail.com>
2019-07-04 13:43:15 +0200

    Update simple-advertising.php
    Internalization

--- simple-advertising.php
+++ simple-advertising.php
@@ -4,11 +4,13 @@
 Plugin Name: Simple Advertising
 Plugin URI: https://github.com/my-language-skills/simple-advertising
 Description: With use of this plugin you will be able to allocate advertisements on all of your post types. Only for multisite installations!(single site soon)
-Version: 1.0.1
+Version: 1.0.2
 Author: My Language Skills team
 Author URI: https://github.com/my-language-skills
 License: GPL 3.0
 Network: True
+Text Domain: simple-advertising
+Domain Path: /languages
 */
 
 // If this file is called directly, abort.
@@ -26,10 +28,11 @@ if ( ! defined( 'WPINC' ) ) {
 function smplads_network_set_page(){
 
 	//creating network administration page
-	add_submenu_page('settings.php', 'Simple Advirtising', 'Simple Advertising', 'manage_network_options', 'smplads_net_settings', 'smplads_render_net_sett');
+	add_submenu_page('settings.php', __('Simple Advirtising', 'simple-advertising'),
+									__('Simple Advertising', 'simple-advertising'), 'manage_network_options', 'smplads_net_settings', 'smplads_render_net_sett');
 
 	//adding section to main options page
-	add_settings_section('smplads_locations', 'Advertisement Locations', '', 'smplads_net_settings');
+	add_settings_section('smplads_locations', __('Advertisement Locations', 'simple-advertising'), '', 'smplads_net_settings');
 
 	//getting all public post types, for Pressbooks installation only CPTs from Pressbooks will be used
 	if (is_plugin_active('pressbooks/pressbooks.php')){
@@ -46,53 +49,58 @@ function smplads_network_set_page(){
 		$render_locations = function () use ($post_type){
 			?>
 			<label for="<?=$post_type.'_ads_before'?>">
-				Before <input type="checkbox" name="<?=$post_type.'_ads_before'?>" value="1" id="<?=$post_type.'_ads_before'?>" <?=checked(1, get_site_option($post_type.'_ads_before'))?>>
+				<?php _e('Before', 'simple-advertising');?>
+				<input type="checkbox" name="<?=$post_type.'_ads_before'?>" value="1" id="<?=$post_type.'_ads_before'?>" <?=checked(1, get_site_option($post_type.'_ads_before'))?>>
 			</label>
 			<label style="margin-left: 20px" for="<?=$post_type.'_ads_after'?>">
-				After <input type="checkbox" name="<?=$post_type.'_ads_after'?>" value="1" id="<?=$post_type.'_ads_after'?>" <?=checked(1, get_site_option($post_type.'_ads_after'))?>>
+				<?php _e('After', 'simple-advertising');?> <input type="checkbox" name="<?=$post_type.'_ads_after'?>" value="1" id="<?=$post_type.'_ads_after'?>" <?=checked(1, get_site_option($post_type.'_ads_after'))?>>
 			</label>
 			<?php
 		};
-		add_settings_field($post_type.'_ads', 'In '.ucfirst($post_type).' Content', $render_locations, 'smplads_net_settings', 'smplads_locations');
+		add_settings_field($post_type.'_ads',
+				sprintf(__('In %s Content', 'simple-advertising'), ucfirst($post_type)),
+				$render_locations, 'smplads_net_settings', 'smplads_locations');
 	}
 
 	//creating section for ads content
-	add_settings_section('smplads_contents', 'Advertisement Content', '', 'smplads_net_settings');
+	add_settings_section('smplads_contents',
+												__('Advertisement Content', 'simple-advertising'),
+	 											'', 'smplads_net_settings');
 	register_setting('smplads_net_settings', 'smplads_content_before');
 	register_setting('smplads_net_settings', 'smplads_content_after');
 	register_setting('smplads_net_settings', 'smplads_content_link_before');
 	register_setting('smplads_net_settings', 'smplads_content_link_after');
 
 	//adding content fields
-	add_settings_field('smplads_content_before', '"Before" image (source URL):', function (){
+	add_settings_field('smplads_content_before', __('"Before" image (source URL):', 'simple-advertising'), function (){
 		?>
 			<input type="url" style="width: 50%;" placeholder="www.example.com/wp-content/uploads/2018/01/image-example.jpg" id="smplads_content_before" name="smplads_content_before" value="<?=get_site_option('smplads_content_before') ?: ''?>">
 		<?php
 	}, 'smplads_net_settings', 'smplads_contents');
 
 
-	add_settings_field('smplads_content_link_before', 'External link (if filled in, image becomes a link):', function (){
+	add_settings_field('smplads_content_link_before', __('External link (if filled in, image becomes a link):', 'simple-advertising'), function (){
 		?>
 			<input type="url" style="width: 50%;" placeholder="www.example.com/link-to-the-post" id="smplads_content_link_before" name="smplads_content_link_before" value="<?=get_site_option('smplads_content_link_before') ?: ''?>">
 		<?php
 	}, 'smplads_net_settings', 'smplads_contents');
 
-	add_settings_field('smplads_content_after', '"After" image (source URL):', function (){
+	add_settings_field('smplads_content_after', __('"After" image (source URL):', 'simple-advertising'), function (){
 		?>
 		<input type="url" style="width: 50%;" placeholder="www.example.com/wp-content/uploads/2018/01/image-example.jpg" id="smplads_content_after" name="smplads_content_after" value="<?=get_site_option('smplads_content_after') ?: ''?>">
 		<?php
 	}, 'smplads_net_settings', 'smplads_contents');
 
-	add_settings_field('smplads_content_link_after', 'External link (if filled in, image becomes a link):', function (){
+	add_settings_field('smplads_content_link_after', __('External link (if filled in, image becomes a link):', 'simple-advertising'), function (){
 		?>
 			<input type="url" style="width: 50%;" placeholder="www.example.com/link-to-the-post" id="smplads_content_link_after" name="smplads_content_link_after" value="<?=get_site_option('smplads_content_link_after') ?: ''?>">
 		<?php
 	}, 'smplads_net_settings', 'smplads_contents');
 
 	//add section for main blog
-	add_settings_section('smplads_main_blog', 'Main blog', '', 'smplads_net_settings');
+	add_settings_section('smplads_main_blog', __('Main blog', 'simple-advertising'), '', 'smplads_net_settings');
 	register_setting('smplads_net_settings', 'smplads_main_out');
-	add_settings_field('smplads_main_out', 'No showing Ads', function (){
+	add_settings_field('smplads_main_out', __('No showing Ads', 'simple-advertising'), function (){
 		?>
 		<input type="checkbox" value = 1 name="smplads_main_out" id="smplads_main_out" <?= checked(1,  get_site_option('smplads_main_out'))?>>
 		<?php
@@ -110,7 +118,7 @@ function smplads_render_net_sett(){
 	?>
 	<div class="wrap">
 		<div class="notice updated is-dismissible">
-				<p><strong>Settings saved.</strong></p>
+				<p><strong> <?php _e('Settings saved.', 'simple-advertising'); ?> </strong></p>
 		</div>
 		<form method="POST" action="edit.php?action=update_network_options_ads">
 			<?php
@@ -211,3 +219,23 @@ add_action('network_admin_menu', 'smplads_network_set_page');
 add_action('network_admin_edit_update_network_options_ads', 'smplads_update_options_ads');
 //output of ads
 add_filter('the_content', 'smplads_output_ads', 20, 1);
+
+/**
+ * For Internalization
+ * It loads the MO file for plugin's translation
+ *
+ * @param void
+ *
+ * @since 1.0.2
+ * @author Davide Cazzorla @davideC00
+ *
+ * @return void
+ */
+	function smplads_load_plugin_textdomain() {
+    load_plugin_textdomain( 'simple-advertising', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+/**
+ * For Internalization
+ * Called when the plugin activated has been loaded
+ */
+add_action( 'plugins_loaded', 'smplads_load_plugin_textdomain' );

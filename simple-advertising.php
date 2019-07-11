2ffd6ee30c74c681494c23a6a05f9e912d462c36
Antonio Devís <colomett@gmail.com>
2019-07-11 14:52:32 +0200

    Annotation

--- simple-advertising.php
+++ simple-advertising.php
@@ -23,6 +23,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain:       simple-advertising
  * Domain Path:       /languages
+ * Network: 					True
  */
 
 // If this file is called directly, abort.
